2a2b9b6ba81c189d8c006425b9c697f25319a628
Kevin Dulche Laptop GMR Windows <kevdulche@gmail.com>
2024-03-19 22:38:40 -0600

    Arreglar Codigo de Laboratorio 2 tiempos

--- Laboratorio02/EscribirCSV.java
+++ Laboratorio02/EscribirCSV.java
@@ -19,13 +19,13 @@ public class EscribirCSV {
             Lista<Integer> lista = new Lista<>();
 
             // Crear lista
-            for (Integer i = 0; i < listaN[j]; i++) {
+            for (int i = 0; i < listaN[j]; i++) {
                 lista.insertar(i);
             }
 
             // Acceso
             startTime = System.nanoTime();
-            lista.acceder(listaN[j]-1);
+            lista.acceder(listaN[j]-2);
             endTime = System.nanoTime();
             
             long timeElapsedAccesoLista = endTime - startTime;
@@ -33,7 +33,7 @@ public class EscribirCSV {
 
             // Busqueda
             startTime = System.nanoTime();
-            lista.buscar(listaN[j]);
+            lista.buscar(listaN[j]-1);
             endTime = System.nanoTime();
 
             long timeElapsedBusquedaLista = endTime - startTime;
@@ -55,7 +55,6 @@ public class EscribirCSV {
 
             // Pilas
             Pila<Integer> pila = new Pila<>();
-            Pila<Integer> pilaTemp = new Pila<>();
 
             // Crear pila
             for (Integer i = 0; i < listaN[j]; i++) {
@@ -64,36 +63,10 @@ public class EscribirCSV {
 
             // Acceso
             startTime = System.nanoTime();
-            int finalPila = 0;
-
-            while (!pila.estaVacia()) {
-                finalPila = pila.desapilar();
-                pilaTemp.apilar(finalPila);
-            }
-
+            pila.cima();
             endTime = System.nanoTime();
 
             long timeElapsedAccesoPila = endTime - startTime;
-            
-            while (!pilaTemp.estaVacia()) {
-                pila.apilar(pilaTemp.desapilar());
-            }
-
-            // Busqueda
-            startTime = System.nanoTime();
-            int elemento_a_buscar = 0;
-
-            while ((!pila.estaVacia()) && (pila.cima() != elemento_a_buscar)) {
-                pilaTemp.apilar(pila.desapilar());
-            }
-
-            endTime = System.nanoTime();
-
-            long timeElapsedBusquedaPila = endTime - startTime;
-
-            while (!pilaTemp.estaVacia()) {
-                pila.apilar(pilaTemp.desapilar());
-            }
 
             // Insersion
             startTime = System.nanoTime();
@@ -111,30 +84,18 @@ public class EscribirCSV {
 
             // Colas
             Cola<Integer> cola = new Cola<>();
-            Cola<Integer> colaTemp = new Cola<>();
 
             // Crear cola
             for (Integer i = 0; i < listaN[j]; i++) {
                 cola.encolar(i);
             }
 
-            // Acceso y Busqueda
+            // Acceso
             startTime = System.nanoTime();
-            int ultimo_elemento = listaN[j];
-            
-            while (!cola.estaVacia()){
-                ultimo_elemento = cola.desencolar();
-                colaTemp.encolar(ultimo_elemento);
-            }
-
+            cola.frente();
             endTime = System.nanoTime();
 
             long timeElapsedAccesoCola = endTime - startTime;
-            long timeElapsedBusquedaCola = timeElapsedAccesoCola;
-
-            while (!colaTemp.estaVacia()) {
-                cola.encolar(colaTemp.desencolar());
-            }
 
             // Insersion
             startTime = System.nanoTime();
@@ -150,17 +111,6 @@ public class EscribirCSV {
 
             long timeElapsedEliminacionCola = endTime - startTime;
 
-            // long startTime = System.nanoTime();
-            
-            // for (Integer i = 0; i < 100; i++) {
-            //     lista.insertar(i);
-            // }
-            
-            // long endTime = System.nanoTime();
-            
-            // long timeElapsed = endTime - startTime;
-
-            // System.out.println("Tiempo de ejecucion: " + timeElapsed + " nanosegundos");
 
             try {
                 // Crear un BufferedWriter para escribir en el archivo
@@ -171,8 +121,8 @@ public class EscribirCSV {
 
                 // Escribir datos
                 writer.write("Lista," + timeElapsedAccesoLista + "ns," + timeElapsedBusquedaLista + "ns,"+ timeElapsedInsersionLista + "ns," + timeElapsedEliminacionLista + "ns\n");
-                writer.write("Pila," + timeElapsedAccesoPila + "ns," + timeElapsedBusquedaPila + "ns,"+ timeElapsedInsersionPila + "ns," + timeElapsedEliminacionPila + "ns\n");
-                writer.write("Cola," + timeElapsedAccesoCola + "ns," + timeElapsedBusquedaCola + "ns,"+ timeElapsedInsersionCola + "ns," + timeElapsedEliminacionCola + "ns\n");
+                writer.write("Pila," + timeElapsedAccesoPila + "ns," + "-,"+ timeElapsedInsersionPila + "ns," + timeElapsedEliminacionPila + "ns\n");
+                writer.write("Cola," + timeElapsedAccesoCola + "ns," + "-,"+ timeElapsedInsersionCola + "ns," + timeElapsedEliminacionCola + "ns\n");
 
                 // Cerrar el BufferedWriter
                 writer.close();
--- PracticaArbolesBinariosNB/Pruebas.java
+++ PracticaArbolesBinariosNB/Pruebas.java
@@ -9,26 +9,47 @@ public class Pruebas {
         HashSet<Integer> numerosGenerados = new HashSet<>();
 
         // Número de elementos aleatorios únicos que deseas agregar al árbol
-        int cantidadElementos = 2147483647;
+        // int cantidadElementos = 2147483647;
+        int cantidadElementos = 100000;
 
+        // Medir el tiempo que tarda en agregar elementos al árbol en segundos
+        long startTimeAgregar = System.nanoTime();
         // Agregar números aleatorios únicos al árbol
         while (numerosGenerados.size() < cantidadElementos) {
             int valor = rand.nextInt(cantidadElementos); // Genera números aleatorios entre 0 y 99
             if (!numerosGenerados.contains(valor)) {
                 arbol.insert(valor);
                 numerosGenerados.add(valor);
-                System.out.println("Agregando " + valor + " al arbol");
+                System.out.println("Agregando " + valor + "\t al arbol");
             }
         }
+        long endTimeAgregar = System.nanoTime();
+
+        long durationAgregar = (endTimeAgregar - startTimeAgregar);
+
+        double durationAgregarSeconds = durationAgregar / 1_000_000_000.0;
+        System.out.println("Tiempo de agregar: " + durationAgregarSeconds + " segundos");
+
+        // Medir el tiempo que tarda en buscar elementos al árbol
+        // Buscar un número en el árbol
+        int numeroABuscar = 54321;
+        long startTime = System.nanoTime();
+        System.out.println("Buscando " + numeroABuscar + " ten el arbol");
+        System.out.println(arbol.search(numeroABuscar));
+        long endTime = System.nanoTime();
+
+        long duration = (endTime - startTime);
+
+        System.out.println("Tiempo de busqueda: " + duration + " nanosegundos");
 
         // Imprimir el árbol
-        System.out.println("Preorden:");
-        arbol.preorderTraversal(arbol.getRoot());
+        // System.out.println("Preorden:");
+        // arbol.preorderTraversal(arbol.getRoot());
         
-        System.out.println("\nInorden:");
-        arbol.inorderTraversal(arbol.getRoot());
+        // System.out.println("\nInorden:");
+        // arbol.inorderTraversal(arbol.getRoot());
 
-        System.out.println("\nPostorden:");
-        arbol.posorderTraversal(arbol.getRoot());
+        // System.out.println("\nPostorden:");
+        // arbol.posorderTraversal(arbol.getRoot());
     }
 }
