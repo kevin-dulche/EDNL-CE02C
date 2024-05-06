d3d8f1daa73ebcb32f0da4debdb7175fa3e34767
Kevin Dulche Laptop GMR Windows <kevdulche@gmail.com>
2024-05-06 15:31:51 -0600

    Fin de examen 2

--- SegundoExamenPractica/RepresentacionGrafos.java
+++ SegundoExamenPractica/RepresentacionGrafos.java
@@ -1,58 +1,71 @@
 /*
- * Sección 3: Representación de grafos
-    Pregunta b (25 puntos):
-    Dada una representación de un grafo no dirigido, implemente un algoritmo para recorrer el grafo en anchura (BFS).
-    El algoritmo debe recorrer todos los nodos del grafo y marcarlos como visitados. 
-    La implementación debe indicar si el grafo es conexo.
- */
+* Sección 3: Representación de grafos
+Pregunta b (25 puntos):
+Dada una representación de un grafo no dirigido, implemente un algoritmo para recorrer el grafo en anchura (BFS).
+El algoritmo debe recorrer todos los nodos del grafo y marcarlos como visitados. 
+La implementación debe indicar si el grafo es conexo.
+*/
 
-// ! Pendiente de revisión
-// ? Duda sobre la implementación de la función bfs, siempre me da que el grafo es conexo
-
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class RepresentacionGrafos {
     public static void main(String[] args) {
-        int[][] grafo = {
-            {0, 1, 1, 0, 0, 0},
-            {1, 0, 0, 1, 1, 0},
-            {1, 0, 0, 0, 1, 0},
-            {0, 1, 0, 0, 1, 1},
-            {0, 1, 1, 1, 0, 1},
-            {0, 0, 0, 1, 1, 0}
+        // Asi es conexo
+        int[][] graph = {
+            {0,1,1,0,0,1}, 
+            {1,0,0,0,0,0},
+            {1,0,0,1,1,1},
+            {0,0,1,0,0,0},
+            {0,0,1,0,0,0},
+            {1,0,1,0,0,0}
         };
 
-        //System.out.println(grafo.length);
+        // Asi no es conexo
+        // int[][] graph = {
+        //     {0,0,1,0,0,1}, 
+        //     {0,0,0,0,0,0},
+        //     {1,0,0,1,1,1},
+        //     {0,0,1,0,0,0},
+        //     {0,0,1,0,0,0},
+        //     {1,0,1,0,0,0}
+        // };
 
-        ArrayList<Integer> visitados = new ArrayList<>();
-        for (int i = 0; i < grafo.length; i++) {
-            if (!visitados.contains(i)) {
-                bfs(grafo, i, visitados);
-            }
-        }
-
-        if (visitados.size() == grafo.length) {
-            System.out.println("El grafo es conexo");
-        } else {
-            System.out.println("El grafo no es conexo");
-        }
+        boolean conexo = bfs(graph);
+        System.out.println("El grafo es conexo: " + conexo);
     }
 
-    public static void bfs(int[][] grafo, int nodo, ArrayList<Integer> visitados) {
-        ArrayList<Integer> cola = new ArrayList<>();
-        cola.add(nodo);
-        visitados.add(nodo);
-
-        while (!cola.isEmpty()) {
-            int actual = cola.remove(0);
-            System.out.print(actual + " ");
-
-            for (int i = 0; i < grafo[actual].length; i++) {
-                if (grafo[actual][i] == 1 && !visitados.contains(i)) {
-                    cola.add(i);
-                    visitados.add(i);
+    public static boolean bfs(int[][] graph) {
+        int n = graph.length;
+        boolean[] visited = new boolean[n];
+    
+        // Iniciar la cola para el BFS
+        Queue<Integer> queue = new LinkedList<>();
+    
+        // Empezar desde el primer nodo
+        queue.add(0);
+        visited[0] = true;
+    
+        // Recorrer todos los nodos conectados
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+    
+            // Recorrer todos los nodos adyacentes
+            for (int i = 0; i < n; i++) {
+                // Si el nodo no ha sido visitado y hay una conexión
+                if (!visited[i] && graph[current][i] == 1) {
+                    queue.add(i);
+                    visited[i] = true;
                 }
             }
         }
+    
+        // Verificar si todos los nodos fueron visitados
+        for (boolean v : visited) {
+            if (!v) {
+                return false; // No es conexo
+            }
+        }
+        return true; // Es conexo
     }
 }
